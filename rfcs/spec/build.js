ab92637301342935edcac2293614bf15244a72cd
Michael Staib <michael@chillicream.com>
2024-01-10 17:48:58 +0100

    more renames

--- rfcs/spec/build.js
+++ rfcs/spec/build.js
@@ -1,4 +1,4 @@
 const fs = require("fs");
 const specMarkdown = require("spec-md");
-const html = specMarkdown.html("./spec/Fusion.md");
+const html = specMarkdown.html("./spec/Spec.md");
 fs.writeFile("./SPECIFICATION.html", html, () => {});
